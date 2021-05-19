05810fe1d1a8410b35c6cb154886579182c0f5e0
mnaganur <naganurprasad@yahoo.in>
2021-05-19 12:06:39 +0530

    added viewset

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 from . import serializers
 
@@ -44,3 +44,47 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Handle Delete of an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API view sets"""
+
+    serializer_class = serializers.HelloSerializers
+
+    def list(self, request):
+        """return Hello message"""
+
+        a_viewsets = [
+            'Uses action (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more funcs with less code',
+        ]
+        return Response({'message': 'Hello!', 'a_viewset': a_viewsets})
+
+    def create(self, request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            Response(serializer.errors,
+                     status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'DELETE'})
