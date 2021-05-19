be38e53d34ae3cd6e6b758872efb9cafd5324c7a
mnaganur <naganurprasad@yahoo.in>
2021-05-19 15:26:20 +0530

    added search to user profile

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import permissions
 from . import serializers, models
@@ -99,3 +100,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnerProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
